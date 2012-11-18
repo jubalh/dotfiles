2e7a065fd73beb2fa3da737c39bba4e4c4981768
Michael Vetter <g.bluehut@gmail.com>
2012-11-18 23:05:07 +0100

    added gtk 2/3 settings. and vimperator conf

--- setup.sh
+++ setup.sh
@@ -5,8 +5,11 @@ function link_to(){
 	ln -s ~/.dotfiles/$1 $2
 }
 
+link_to gtkrc-2.0 ~/.gtkrc-2.0
+mkdir -p ~/.config/gtk-3.0
+link_to gtk3settings.ini ~/.config/gtk-3.0/settings.ini
 link_to vimrc ~/.vimrc
-
+link_to vimperatorrc ~/.vimperatorrc
 rm -f ~/.i3/config
 mkdir -p ~/.config/i3
 link_to i3config ~/.config/i3/config
@@ -15,3 +18,4 @@ link_to i3status.conf ~/.config/i3status/config
 link_to xprofile ~/.xprofile
 link_to Xdefaults ~/.Xdefaults
 link_to Xresources ~/.Xresources
+link_to zshrc ~/.zshrc
