ed9b697ed6950ca2f610eb917e36cdfcc65d5ae2
Michael Vetter <g.bluehut@gmail.com>
2015-05-02 10:02:49 +0200

    outcomment gtk and i3 settings

--- setup.sh
+++ setup.sh
@@ -19,15 +19,16 @@ function nix(){
 
 # Linux specific {{{
 if [[ $1 == 'linux' ]] ; then
-	link_to gtkrc-2.0 ~/.gtkrc-2.0
-	mkdir -p ~/.config/gtk-3.0
-	link_to gtk3settings.ini ~/.config/gtk-3.0/settings.ini
-	rm -f ~/.i3/config
-	mkdir -p ~/.config/i3
-	link_to i3config ~/.config/i3/config
-	mkdir -p ~/.config/i3status
-	link_to i3status.conf ~/.config/i3status/config
-	link_to xprofile ~/.xprofile
+
+#	link_to gtkrc-2.0 ~/.gtkrc-2.0
+#	mkdir -p ~/.config/gtk-3.0
+#	link_to gtk3settings.ini ~/.config/gtk-3.0/settings.ini
+#	rm -f ~/.i3/config
+#	mkdir -p ~/.config/i3
+#	link_to i3config ~/.config/i3/config
+#	mkdir -p ~/.config/i3status
+#	link_to i3status.conf ~/.config/i3status/config
+#	link_to xprofile ~/.xprofile
 	link_to Xdefaults ~/.Xdefaults
 	link_to Xresources ~/.Xresources
 	nix
